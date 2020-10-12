41851822eecec5e419bb3ae50906273a9c24c537
Maciej <maciej@contact.apurer.io>
2020-10-12 21:36:27 +0200

    added new payload keys

--- notification.go
+++ notification.go
@@ -30,24 +30,33 @@ type APS struct {
 	// Sound is the name of a sound file to play as an alert.
 	Sound string `json:"sound,omitempty"`
 
-	// Content available apps launched in the background or resumed.
-	ContentAvailable bool `json:"content-available,omitempty"`
+	// ThreadID presents the app-specific identifier for grouping notifications.
+	ThreadID string `json:"thread-id,omitempty"`
 
 	// Category identifier for custom actions in iOS 8 or newer.
 	Category string `json:"category,omitempty"`
 
-	// ThreadID presents the app-specific identifier for grouping notifications.
-	ThreadID string `json:"thread-id,omitempty"`
+	// Content available apps launched in the background or resumed.
+	ContentAvailable *int `json:"content-available,omitempty"`
+
+	// The notification service app extension flag. If the value is 1, the system passes the notification to your notification service app extension before delivery.
+	MutableContent *int `json:"mutable-content,omitempty"`
+
+	// The identifier of the window brought forward. The value of this key will be populated on the UNNotificationContent object created from the push payload.
+	TargetContentId string `json:"target-content-id,omitempty"`
 }
 
 // Alert represents aler dictionary.
 type Alert struct {
-	Title        string   `json:"title,omitempty"`
-	Body         string   `json:"body,omitempty"`
-	TitleLocKey  string   `json:"title-loc-key,omitempty"`
-	TitleLocArgs []string `json:"title-loc-args,omitempty"`
-	ActionLocKey string   `json:"action-loc-key,omitempty"`
-	LocKey       string   `json:"loc-key,omitempty"`
-	LocArgs      []string `json:"loc-args,omitempty"`
-	LaunchImage  string   `json:"launch-image,omitempty"`
+	Title           string   `json:"title,omitempty"`
+	Subtitle        string   `json:"subtitle,omitempty"`
+	Body            string   `json:"body,omitempty"`
+	LaunchImage     string   `json:"launch-image,omitempty"`
+	TitleLocKey     string   `json:"title-loc-key,omitempty"`
+	TitleLocArgs    []string `json:"title-loc-args,omitempty"`
+	SubtitleLocKey  string   `json:"subtitle-loc-key,omitempty"`
+	SubtitleLocArgs []string `json:"subtitle-loc-args,omitempty"`
+	ActionLocKey    string   `json:"action-loc-key,omitempty"`
+	LocKey          string   `json:"loc-key,omitempty"`
+	LocArgs         []string `json:"loc-args,omitempty"`
 }
